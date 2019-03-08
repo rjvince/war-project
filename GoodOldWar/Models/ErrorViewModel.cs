885866e10997785e1e3508c64e418057761b43f0
rjvince <rob.vince@gmail.com>
2019-03-08 11:55:07 -0500

    Add project files.

---
+++ GoodOldWar/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GoodOldWar.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
